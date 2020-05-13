2d909e48308a01e9e3b6c1ac82f1330752cf33a4
prasun2106 <prasunk077@gmail.com>
2020-05-13 21:55:32 +0530

    first commit introduction added

---
+++ list_of_fortune_500_companies.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In this notebook, we will understand the steps of static web scrapping to extract a list from any website in simple steps. Python provides a powerful library BeautifulSoup for web scraping. After extracting the list from internet we will see how to create a list note in notion.
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+
+# In[ ]:
+
+
+
+
